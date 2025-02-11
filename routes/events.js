9390fb9d843d452b5f6449c9695d27b4083924ba
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-02-10 22:34:07 -0600

    new route for event

---
+++ routes/events.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import eventsModel from '../models/eventsSchema.js';
+
+export const eventsRouter = express.Router();
+
+eventsRouter.route("/")
+    .post(async (req,res) => {
+        const newEvent = new eventsModel(req.body);
+        try {
+            const event = await newEvent.save();
+            res.status(201).json(event);
+        } catch (error) {
+            res.status(422).json({ error: "Unable to add new event."})
+        }
+    })
