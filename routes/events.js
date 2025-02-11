2a2a4f043ef34ffc025979c7040c030b39c0b10e
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-02-10 23:55:18 -0600

    error solve

--- routes/events.js
+++ routes/events.js
@@ -3,17 +3,6 @@ import eventsModel from '../models/eventsSchema.js';
 
 export const eventsRouter = express.Router();
 
-// eventsRouter.route("/")
-//     .post(async (req,res) => {
-//         const newEvent = new eventsModel(req.body);
-//         try {
-//             const event = await newEvent.save();
-//             res.status(201).json(event);
-//         } catch (error) {
-//             res.status(422).json({ error: "Unable to add new event."})
-//         }
-//     })
-
 eventsRouter.route("/")
     .post(async (req, res) => {
         const newEvent = new eventsModel(req.body);
@@ -21,7 +10,7 @@ eventsRouter.route("/")
             const event = await newEvent.save();
             res.status(201).json(event);
         } catch (error) {
-            console.error("Error adding event:", error);  // Log the full error
+            console.error("Error adding event:", error);
             res.status(422).json({ error: "Unable to add new event." });
         }
     });
